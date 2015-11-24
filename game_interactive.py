77625dcc4eaab774d3cc40ec6bf9940682f81fd0
John Morgan <jwrm2@cam.ac.uk>
2015-11-24 12:28:20 +0000

    Added buttons for interaction.

--- game_interactive.py
+++ game_interactive.py
@@ -1,28 +1,43 @@
-import game_display
+import game_display as gd
 from gi.repository import Gtk
 
-class GameInteractive(game_display.GameDisplay):
+class GameInteractive(gd.GameDisplay):
   """A game of 2048 with a display and interactive moving."""
 
   def __init__(self):
     """Initialise the game and the display."""
     super(GameInteractive, self).__init__()
-    self._createDisplay()
 
   def _createDisplay(self):
     """Sets up the window ready for display."""
+    self.win = Gtk.Window()
+    self.win.set_title("2048 Interactive")
+    self.win.set_default_size(gd.gridRight + gd.rightBorder, gd.gridBottom + gd.bottomBorder + buttonHeight)
 
-    self.bottomBox = Gtk.Box(spacing = 50)
-    self.add(bottomBox)
+    hbox = Gtk.Box(spacing=6)
+    self.win.add(hbox)
 
-    self.newGameButton = Gtk.Button(label="New Game")
-    self.newGameButton.connect("clicked", self.newGame)
-    self.bottomBox.pack_start(self.newGameButton, True, True, 0)
+    self.da = Gtk.DrawingArea()
+    self.da.set_size_request(gd.gridRight + gd.rightBorder, gd.gridBottom + gd.bottomBorder)
+    self.da.connect("draw", self.updateDisplay)
+    hbox.pack_start(self.da, True, True, 0)
 
-    self.saveButton = Gtk.Button(label="Save & Quit")
-    self.saveButton.connect("clicked", self._saveQuit)
-    self.bottomBox.pack_start(self.saveButton, True, True, 0)
+    button = Gtk.Button.new_with_label("New Game")
+    button.connect("clicked", self._startNew)
+    hbox.pack_start(button, True, True, 0)
+
+    button = Gtk.Button.new_with_label("Save and Quit")
+    button.connect("clicked", self._saveQuit)
+    hbox.pack_start(button, True, True, 0)
+
+    self.win.show_all()
+
+  def _startNew(self):
+    """Starts a new game."""
+    self.newGame()
+    self.updateDisplay()
 
   def _saveQuit(self):
     """Writes the game data to a file and exits."""
 
+buttonHeight = 50
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import game_display
+import game_interactive
 from gi.repository import Gtk
 
-gd = game_display.GameDisplay()
-gd.win.connect('destroy', Gtk.main_quit)
+gi = game_interactive.GameInteractive()
+gi.win.connect('destroy', Gtk.main_quit)
 
 Gtk.main()
