f5d1783191a3b0a5fcacba44ce2aa8ee9b70e358
John Morgan <jwrm2@cam.ac.uk>
2015-12-24 12:00:57 +0000

    Added load saved game capability.

--- game_display.py
+++ game_display.py
@@ -17,7 +17,7 @@ class GameDisplay(game.Game):
         """Sets up the window ready for display."""
         self.win = Gtk.Window()
         self.win.set_title("2048")
-        self.win.set_default_size(gridRight + rightBorder, gridBottom + bottomBorder)
+        self.win.set_default_size(grid_right + right_border, grid_bottom + bottom_border)
 
         self.da = Gtk.DrawingArea()
         self.da.connect("draw", self.update_display)
@@ -35,83 +35,94 @@ class GameDisplay(game.Game):
         del w
 
         # Background color and cleaning
-        cr.set_source_rgb(backgroundColor.get_red_f(), backgroundColor.get_green_f(), backgroundColor.get_blue_f())
+        cr.set_source_rgb(background_color.get_red_f(), background_color.get_green_f(), background_color.get_blue_f())
         cr.paint()
 
+        # The score
+        cr.set_source_rgb(score_color.get_red_f(), score_color.get_green_f(), score_color.get_blue_f())
+        cr.select_font_face("Helvetica", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+        cr.set_font_size(score_size)
+        (x, y, width, height, dx, dy) = cr.text_extents(str(self.score))
+        cr.move_to(grid_right - width - grid_line_width/2, top_border/2 + height/2)
+        cr.show_text(str(self.score))
+
         # The grid
-        cr.set_line_width(gridLineWidth)
-        cr.set_source_rgb(gridColor.get_red_f(), gridColor.get_green_f(), gridColor.get_blue_f())
+        cr.set_line_width(grid_line_width)
+        cr.set_source_rgb(grid_color.get_red_f(), grid_color.get_green_f(), grid_color.get_blue_f())
         for i in range(self.grid_size+1):
-            cr.move_to(gridLeft + i*tileSize + (i-0.5)*gridLineWidth, gridTop - gridLineWidth)
-            cr.line_to(gridLeft + i*tileSize + (i-0.5)*gridLineWidth, gridBottom - gridLineWidth/2)
+            cr.move_to(grid_left + i * tile_size + (i - 0.5) * grid_line_width, grid_top - grid_line_width)
+            cr.line_to(grid_left + i * tile_size + (i - 0.5) * grid_line_width, grid_bottom - grid_line_width / 2)
             cr.stroke_preserve()
-            cr.move_to(gridLeft - gridLineWidth, gridTop + i*tileSize + (i-0.5)*gridLineWidth)
-            cr.line_to(gridRight - gridLineWidth/2, gridTop + i*tileSize + (i-0.5)*gridLineWidth)
+            cr.move_to(grid_left - grid_line_width, grid_top + i * tile_size + (i - 0.5) * grid_line_width)
+            cr.line_to(grid_right - grid_line_width / 2, grid_top + i * tile_size + (i - 0.5) * grid_line_width)
             cr.stroke_preserve()
 
         # The tiles
         for i in range(self.grid_size):
             for j in range(self.grid_size):
                 if self.grid[i][j] != 0:
-                    color = tileColors[self.grid[i][j]]
+                    color = tile_colors[self.grid[i][j]]
                     cr.set_source_rgb(color.get_red_f(), color.get_green_f(), color.get_blue_f())
-                    cr.rectangle(gridLeft + gridLineWidth*i + tileSize*i,     gridTop + gridLineWidth*j + tileSize*j,
-                                 tileSize, tileSize)
+                    cr.rectangle(grid_left + grid_line_width * i + tile_size * i,
+                                 grid_top + grid_line_width * j + tile_size * j,
+                                 tile_size, tile_size)
                     cr.fill()
 
         # The tile labels
         for i in range(self.grid_size):
             for j in range(self.grid_size):
                 if self.grid[i][j] != 0:
-                    color = labelColor[labelDict[self.grid[i][j]]]
+                    color = label_color[label_dict[self.grid[i][j]]]
                     cr.set_source_rgb(color.get_red_f(), color.get_green_f(), color.get_blue_f())
                     cr.select_font_face("Helvetica", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-                    cr.set_font_size(labelFontSize[self.grid[i][j]])
+                    cr.set_font_size(label_font_size[self.grid[i][j]])
                     (x, y, width, height, dx, dy) = cr.text_extents(str(self.grid[i][j]))
-                    cr.move_to(gridLeft + gridLineWidth*i + tileSize*(i+0.5) - width/2,
-                               gridTop + gridLineWidth*j + tileSize*(j+0.5) + height/2)
+                    cr.move_to(grid_left + grid_line_width * i + tile_size * (i + 0.5) - width / 2,
+                               grid_top + grid_line_width * j + tile_size * (j + 0.5) + height / 2)
                     cr.show_text(str(self.grid[i][j]))
 
 
-backgroundColor = utils.HexColor("0xEDEDED")
-gridColor = utils.HexColor("0xBBADA0")
-squareColor = utils.HexColor("0xCDC1B4")
-labelColor = [utils.HexColor("0x776E65"), utils.HexColor("0xF9F6F2")]
-labelDict = {2**i: 1 for i in range(3, 18)}
-labelDict[2] = 0
-labelDict[4] = 0
-labelFontSize = {2: 60, 4: 60, 8: 60,
-                 16: 40, 32: 40, 64: 40,
-                 128: 30, 256: 30, 512: 30,
-                 1024: 25, 2048: 25, 4096: 25, 8192: 25,
-                 16384: 20, 32768: 20, 65536: 20,
-                 131072: 20}
-tileColors = {2: utils.HexColor("0xEEE4DA"),
-              4: utils.HexColor("0xEDE0C8"),
-              8: utils.HexColor("0xF2B179"),
-              16: utils.HexColor("0xF59563"),
-              32: utils.HexColor("0xF67C5F"),
-              64: utils.HexColor("0xF65E3B"),
-              128: utils.HexColor("0xEDCF72"),
-              256: utils.HexColor("0xEDCC61"),
-              512: utils.HexColor("0xEDC850"),
-              1024: utils.HexColor("0xEDC53F"),
-              2048: utils.HexColor("0xEDC22E"),
-              4096: utils.HexColor("0x3C3A32"),
-              8192: utils.HexColor("0x3C3A32"),
-              16384: utils.HexColor("0x3C3A32"),
-              32768: utils.HexColor("0x3C3A32"),
-              65536: utils.HexColor("0x3C3A32"),
-              131072: utils.HexColor("0x3C3A32")}
+background_color = utils.HexColor("0xEDEDED")
+grid_color = utils.HexColor("0xBBADA0")
+score_color = utils.HexColor("0x776E65")
+score_size = 40
+square_color = utils.HexColor("0xCDC1B4")
+label_color = [utils.HexColor("0x776E65"), utils.HexColor("0xF9F6F2")]
+label_dict = {2 ** i: 1 for i in range(3, 18)}
+label_dict[2] = 0
+label_dict[4] = 0
+label_font_size = {2: 60, 4: 60, 8: 60,
+                   16: 40, 32: 40, 64: 40,
+                   128: 30, 256: 30, 512: 30,
+                   1024: 25, 2048: 25, 4096: 25, 8192: 25,
+                   16384: 20, 32768: 20, 65536: 20,
+                   131072: 20}
+tile_colors = {2: utils.HexColor("0xEEE4DA"),
+               4: utils.HexColor("0xEDE0C8"),
+               8: utils.HexColor("0xF2B179"),
+               16: utils.HexColor("0xF59563"),
+               32: utils.HexColor("0xF67C5F"),
+               64: utils.HexColor("0xF65E3B"),
+               128: utils.HexColor("0xEDCF72"),
+               256: utils.HexColor("0xEDCC61"),
+               512: utils.HexColor("0xEDC850"),
+               1024: utils.HexColor("0xEDC53F"),
+               2048: utils.HexColor("0xEDC22E"),
+               4096: utils.HexColor("0x3C3A32"),
+               8192: utils.HexColor("0x3C3A32"),
+               16384: utils.HexColor("0x3C3A32"),
+               32768: utils.HexColor("0x3C3A32"),
+               65536: utils.HexColor("0x3C3A32"),
+               131072: utils.HexColor("0x3C3A32")}
 
-gridLineWidth = 20
-tileSize = 80
-topBorder = 50 + gridLineWidth
-bottomBorder = 50
-leftBorder = 50 + gridLineWidth
-rightBorder = 50
+grid_line_width = 20
+tile_size = 80
+top_border = 50 + grid_line_width
+bottom_border = 50
+left_border = 50 + grid_line_width
+right_border = 50
 
-gridLeft = leftBorder + gridLineWidth/2
-gridRight = leftBorder + 4*tileSize + 5*gridLineWidth
-gridTop = topBorder + gridLineWidth/2
-gridBottom = topBorder + 4*tileSize + 5*gridLineWidth
+grid_left = left_border + grid_line_width / 2
+grid_right = left_border + 4 * tile_size + 5 * grid_line_width
+grid_top = top_border + grid_line_width / 2
+grid_bottom = top_border + 4 * tile_size + 5 * grid_line_width
--- game_interactive.py
+++ game_interactive.py
@@ -1,8 +1,8 @@
 from gi.repository import Gtk
 from gi.repository import Gdk
 import os
-from simplecrypt import encrypt, decrypt
-from utils import get_password
+import simplecrypt as sc
+from utils import get_password, is_power
 
 import game
 import game_display as gd
@@ -21,14 +21,14 @@ class GameInteractive(gd.GameDisplay):
         """Sets up the window ready for display."""
         self.win = Gtk.Window()
         self.win.set_title("2048 Interactive")
-        self.win.set_default_size(gd.gridRight + gd.rightBorder, gd.gridBottom + gd.bottomBorder + buttonHeight)
+        self.win.set_default_size(gd.grid_right + gd.right_border, gd.grid_bottom + gd.bottom_border + button_height)
         self.win.connect("key-press-event", self._key_pressed)
 
         grid = Gtk.Grid()
         self.win.add(grid)
 
         da = Gtk.DrawingArea()
-        da.set_size_request(gd.gridRight + gd.rightBorder, gd.gridBottom + gd.bottomBorder)
+        da.set_size_request(gd.grid_right + gd.right_border, gd.grid_bottom + gd.bottom_border)
         da.connect("draw", self.update_display)
         grid.attach(da, 0, 0, 2, 1)
 
@@ -50,6 +50,28 @@ class GameInteractive(gd.GameDisplay):
 
     def _load_save(self):
         """Loads the saved game file."""
+        try:
+            load = open(save_file, 'r')
+            cipher_text = load.read()
+            load_string = sc.decrypt(get_password(), cipher_text)
+            load_values = load_string.splitlines()
+            if len(load_values) != self.grid_size*self.grid_size + 1:
+                raise ValueError
+            self.score = int(load_values[0])
+            for i in range(self.grid_size):
+                for j in range(self.grid_size):
+                    self.grid[i][j] = int(load_values[i*self.grid_size + j + 1])
+                    if not is_power(self.grid[i][j], 2) and self.grid[i][j] != 0:
+                        raise ValueError
+        except sc.DecryptionException:
+            print("Save file could not be read. Starting new game")
+            self.new_game()
+        except ValueError:
+            print("Save file format corrupted. Starting new game")
+            self.new_game()
+
+        os.remove(save_file)
+        self.win.queue_draw()
 
     def _key_pressed(self, widget, event):
         """Deals with keys pressed to make moves"""
@@ -77,12 +99,12 @@ class GameInteractive(gd.GameDisplay):
             for j in range(self.grid_size):
                 save_string += str(self.grid[i][j]) + '\n'
 
-        cipher_text = encrypt(get_password(), save_string)
+        cipher_text = sc.encrypt(get_password(), save_string)
         save.write(cipher_text)
         save.close()
 
         Gtk.main_quit()
 
 
-buttonHeight = 30
+button_height = 30
 save_file = "save_game"
