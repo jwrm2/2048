0a86572c0698cabf75aeab4442f9d49eec3c387d
John Morgan <jwrm2@cam.ac.uk>
2015-12-25 13:07:06 +0000

    Added genetic simulation. Not tested.

--- controller.py
+++ controller.py
@@ -3,6 +3,7 @@ from numpy import where
 from pybrain.structure import FeedForwardNetwork
 from pybrain.structure import FullConnection
 from pybrain.structure import LinearLayer, SigmoidLayer
+import random
 
 
 class Controller(object):
@@ -11,8 +12,8 @@ class Controller(object):
     def __init__(self, grid_size, hidden_list):
         """Sets up the neural network.
 
-        :param grid_size: the size of the grid, for specifying the input layer.
-        :param hidden_list: a list containing the number of nodes in each hidden layer.
+        @param grid_size: the size of the grid, for specifying the input layer.
+        @param hidden_list: a list containing the number of nodes in each hidden layer.
         """
         self.net = FeedForwardNetwork()
 
@@ -35,24 +36,44 @@ class Controller(object):
 
         self.net.sortModules()
 
+    def get_fitness(self, number):
+        """
+        Runs several games to get an average score for the fitness.
 
-def run_game(controller):
-    """Runs a game using the specified controller and returns the score.
+        @param number: number of games to average over
+        @return fitness: the average score
+        """
+        fitness = 0
+        for i in range(number):
+            fitness += self.run_game()
+        fitness /= number
+        return fitness
 
-    :param controller: a neural net to generate the moves
-    :returns the score of the game
-    """
-    g = game.Game()
+    def mutate(self, drift):
+        """Performs a mutation by randomly altering each parameter in the net.
 
-    while g.get_playing():
-        input_list = [item for sub in g.grid for item in sub]
-        decision = controller.net.activate(input_list)
-        for i in range(4):
-            direction = where(decision == max(decision))[0][0]
-            if g.test_move(game.moves[direction]):
-                g.make_move(game.moves[direction])
-                break
-            else:
-                decision[direction] = -100000
+        @param drift scales the size of mutation to apply
+        """
+        for i in range(self.net.params.size):
+            self.net.params[i] += (random.random() - 1) * drift
 
-    return g.get_score()
+    def run_game(self):
+        """Runs a game and returns the score.
+
+        @return the score of the game
+        """
+        g = game.Game()
+
+        while g.get_playing():
+            input_list = [item for sub in g.grid for item in sub]
+            decision = self.net.activate(input_list)
+            for i in range(4):
+                direction = where(decision == max(decision))[0][0]
+                if g.test_move(game.moves[direction]):
+                    g.make_move(game.moves[direction])
+                    break
+                else:
+                    decision[direction] = -100000
+
+        return g.get_score()
+
---
+++ genetic.py
@@ -0,0 +1,94 @@
+import controller
+import random
+
+
+def breed(parent1, parent2, sigma):
+    """
+    Breeds two controllers to create a child.
+    The dimensions of the nets of the two parents must match.
+    The parameter of the child is drawn from a normal distribution, with mean given by the average of the
+    parental values and variance as specified.
+
+    :param parent1: first parent
+    :param parent2: second parent
+    :param sigma: standard deviation of the normal distribution
+    :returns child: a blending of the two parents
+    """
+    if parent1.net.params.shape != parent2.net.params.shape:
+        raise ValueError("Can't breed nets: they are not the same shape")
+    child = c.Controller(1, [1])
+    child.net = parent1.net.copy()
+    for i in range(child.net.params.size):
+        mu = (parent1.net.params[i] + parent2.net.params[i]) / 2
+        child.net.params[i] = random.gauss(mu, sigma)
+    child.net.sortModules()
+    return child
+
+
+def run_breeding(pop, sel, grid_size, hidden_list, drift, sigma, gen, num):
+    """
+    Runs a complete simulation, breeding the nets.
+
+    @param pop: number of nets in each generation
+    @param sel: number of nets that survive to breed
+    @param grid_size: dimension of the 2048 grid
+    @param hidden_list: shape of the hidden layers in the net
+    @param drift: determines how much asexual variation in each child
+    @param sigma: determines how much sexual variation in each child
+    @param gen: number of generation to run
+    @param num: number of runs to average over when calculating fitness
+    """
+    # Generate the initial population
+    population = []
+    for i in range(pop):
+        population.append(controller.Controller(grid_size, hidden_list))
+
+    best_fitness = 0
+    best_params = []
+
+    # Loop over the number of generations
+    for i in range(gen):
+        print "BEGINNING GENERATION " + str(i)
+
+        pop_best_fitness = 0
+        pop_best_params = []
+
+        # Get fitness for each controller
+        fitnesses = {}
+        for c in population:
+            fitnesses[c.get_fitness()] = c
+
+        # Select the best controllers
+        breeding_population = []
+        for key in sorted(fitnesses, reverse=True):
+            if len(breeding_population) >= sel:
+                break
+            breeding_population.append(fitnesses[key])
+            print "Adding controller with fitness " + str(key) + " to breeding population"
+            if key > pop_best_fitness:
+                pop_best_fitness = key
+                pop_best_params = fitnesses[key].net.params
+                print "New best fitness: " + str(best_fitness)
+            if key > best_fitness:
+                best_fitness = key
+                best_params = fitnesses[key].net.params
+                print "New best fitness: " + str(best_fitness)
+
+        # Create the children by randomly selecting breeding pairs
+        population = []
+        while len(population) < pop:
+            p1 = random.randint(0, len(breeding_population) - 1)
+            p2 = p1
+            while p2 == p1:
+                p2 = random.randint(0, len(breeding_population) - 1)
+            print "Breeding child from parent " + str(p1) + " and " + str(p2)
+            child = breed(breeding_population[p1], breeding_population[p2], sigma)
+            child.mutate(drift)
+
+        print "Best population fitness: " + str(pop_best_fitness)
+        print "Best population parameters: " + str(pop_best_params)
+        print "Best fitness: " + str(best_fitness)
+        print "Best parameters: " + str(best_params)
+        print "END GENERATION " + str(i)
+
+
